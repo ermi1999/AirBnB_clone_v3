8889b2b01ac17a16eacb74335d165f9cfe699320
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-25 12:57:18 +0300

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={"/api/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """tears down the app whe the app closes"""
